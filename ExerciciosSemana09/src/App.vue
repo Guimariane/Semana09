afed4dfb37305b664ceed1c4ebebe1c583d13dbe
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-09-17 03:12:57 -0300

    Baixando o arquivo
    Baixando o vue no repositório

---
+++ ExerciciosSemana09/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ ExerciciosSemana09/src/components/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template>
+  <header>
+    <img>
+    <h1>Farmárcia</h1>
+  </header>
+</template>
