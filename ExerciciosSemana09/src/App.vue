b263c3c2deea28bb991762cd7c7d25fc836ad7aa
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-09-17 04:32:10 -0300

    Função Adicionada
    Adição da Função Adicionar Medicamento

--- ExerciciosSemana09/src/App.vue
+++ ExerciciosSemana09/src/App.vue
@@ -8,6 +8,26 @@ import Header from '../src/components/Header.vue';
 import Formulario from '../src/components/FormularioNovoMedicamento.vue'
 
 export default {
+  data() {
+    listaMedicamentos: []
+  },
+
+  method:{
+    AdicionarMedicamento(nome, laboratorio, preco){
+      if (nome == '' || laboratorio == '' || preco == 0){
+        alert("Dados obrigatórios")
+        return
+      }
+      const novoMedicamento = {
+        id: 1,
+        nome: MedName,
+        laboratorio: LabName,
+        preco: Price,
+        favorito: false
+      }
+      this.listaMedicamentos.push(novoMedicamento)
+    }
+  },
   components: {
     Header,
     Formulario
